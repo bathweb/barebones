ec2ffc4e2ad40d7c6eeba87c8a4aa5d0ab6a34ec
University of Bath Digital Team <web-support@bath.ac.uk>
2014-04-10 21:30:08 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html><html lang="en-gb">
 
 <head>
-	<title>Barebones - An initial directory setup, style guide and pattern primer</title>
+	<title>University of Bath - An initial directory setup, style guide and pattern primer</title>
 	<meta charset="utf-8"/>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
